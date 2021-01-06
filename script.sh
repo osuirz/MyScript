14f7771a8f3bece1c8b32a0ab4ddbf76092c0006
KoddyDev <73896941+KoddyDev@users.noreply.github.com>
2021-01-06 16:43:56 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,8 +11,7 @@ output "==================="
     if [ "$EUID" -ne 0 ]; then
         output "Execute com root."
         exit 3
-      fi
-        
+      fi 
 }
 options(){
     output "Selecione uma opção:"
