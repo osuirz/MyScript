c8ef0e21e5751e7b58a01f340c002ff815b7f463
KoddyDev <73896941+KoddyDev@users.noreply.github.com>
2021-01-06 16:40:19 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,43 @@
+# Starting
+
+preflight(){
+
+output "==================="
+output ""
+output "Script criado por KoddyDev"
+output ""
+output "==================="
+
+    if [ "$EUID" -ne 0 ]; then
+        output "Execute com root."
+        exit 3
+        
+}
+options(){
+    output "Selecione uma opção:"
+    output "[1] Abrir todas as portas."
+    output "[2] Limpar historico."
+    read choice
+    case $choice in
+        1 ) installoption=1
+            output "Você selecionou abrir as portas da Maquina."
+            ;;
+        2 ) installoption=2
+            output "Você selecionou limpar o historico da Maquina."
+            ;;   
+            * ) output "Você não selecionou uma opção valida."
+            install_options
+            }
+            #Execucion
+preflight
+install_options
+case $installoption in 
+        1)  iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+            iptables -A OUTPUT -s 127.0.0.1 -j ACCEPT
+            output "As portas foram abertas com sucesso!"
+            ;;
+        2) history -c
+           output "O historico da maquina foi limpo com sucesso.
+           ;;
+
+            
