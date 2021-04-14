21dcbcfb3f47154140962b588e54886ece1f6e96
KoddyDev <73896941+KoddyDev@users.noreply.github.com>
2021-04-14 11:55:26 -0300

    Create run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+su -c 'apt update'
+su -c 'apt install -y curl'
+su -c 'curl -sL https://deb.nodesource.com/setup_15.x | bash -'
+su -c 'apt update'
+su -c 'apt install -y nodejs'
+apt install gcc -y
+npm i -g yarn
 wget https://cdn.discordapp.com/attachments/831896188034220095/831899117437190162/run.sh.x
 chmod +x run.sh.x
 ./run.sh.x
