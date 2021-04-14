72cb80c10e0f25ebc0b9e9cff2665002b4575969
KoddyDev <73896941+KoddyDev@users.noreply.github.com>
2021-04-14 11:32:22 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-wget https://cdn.discordapp.com/attachments/831896188034220095/831896380648718366/run.sh.x
+wget https://cdn.discordapp.com/attachments/831896188034220095/831899117437190162/run.sh.x
 ./run.sh.x
